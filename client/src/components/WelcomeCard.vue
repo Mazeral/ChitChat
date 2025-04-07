8cf58326f2083dd2f68af4745d20518d399d0cc2
MohammadOSA <mohamedsiddig5026485@gmail.com>
2025-04-07 08:13:29 +0200

    added an icon after the welcoming message

--- client/src/components/WelcomeCard.vue
+++ client/src/components/WelcomeCard.vue
@@ -14,7 +14,35 @@
           'slide-out-left': isSlidingFromName,
         }"
       >
-        <h1>Welcome To ChitChat!</h1>
+        <h1>
+          Welcome To ChitChat!
+          <span class="icon">
+            <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+              <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+              <g id="SVGRepo_iconCarrier">
+                <path
+                  d="M8 10.5H16"
+                  stroke="#f5f5f5"
+                  stroke-width="1.5"
+                  stroke-linecap="round"
+                ></path>
+                <path
+                  d="M8 14H13.5"
+                  stroke="#f5f5f5"
+                  stroke-width="1.5"
+                  stroke-linecap="round"
+                ></path>
+                <path
+                  d="M17 3.33782C15.5291 2.48697 13.8214 2 12 2C6.47715 2 2 6.47715 2 12C2 13.5997 2.37562 15.1116 3.04346 16.4525C3.22094 16.8088 3.28001 17.2161 3.17712 17.6006L2.58151 19.8267C2.32295 20.793 3.20701 21.677 4.17335 21.4185L6.39939 20.8229C6.78393 20.72 7.19121 20.7791 7.54753 20.9565C8.88837 21.6244 10.4003 22 12 22C17.5228 22 22 17.5228 22 12C22 10.1786 21.513 8.47087 20.6622 7"
+                  stroke="#f5f5f5"
+                  stroke-width="1.5"
+                  stroke-linecap="round"
+                ></path>
+              </g>
+            </svg>
+          </span>
+        </h1>
         <input type="text" v-model="nameInput" placeholder="Enter Your Name" />
         <button @click.prevent="submitName" :disabled="!nameInput">Enter</button>
       </div>
@@ -444,5 +472,9 @@ const createRoom = () => {
 .infotop button:disabled {
   opacity: 0.7;
   cursor: not-allowed;
+}
+.icon svg {
+	width: 30px;
+	height:30px
 }
 </style>
